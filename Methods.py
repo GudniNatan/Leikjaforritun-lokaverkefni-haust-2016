892cebe536a1474041cb71ab7db0f43747423bbc
Guðni Natan Gunnarsson <gudni.nathan@gmail.com>
2017-04-06 16:26:33 +0000

    methods

--- Methods.py
+++ Methods.py
@@ -1,54 +1,11 @@
 import pygame
 import vec2d_jdm
 
-""" aspect_scale - Scaling surfaces keeping their aspect ratio
-Raiser, Frank - Sep 6, 2k++
-crashchaos at gmx.net
 
-This is a pretty simple and basic function that is a kind of
-enhancement to pygame.transform.scale. It scales a surface
-(using pygame.transform.scale) but keeps the surface's aspect
-ratio intact. So you will not get distorted images after scaling.
-A pretty basic functionality indeed but also a pretty useful one.
-
-Usage:
-is straightforward.. just create your surface and pass it as
-first parameter. Then pass the width and height of the box to
-which size your surface shall be scaled as a tuple in the second
-parameter. The aspect_scale method will then return you the scaled
-surface (which does not neccessarily have the size of the specified
-box of course)
-
-Dependency:
-a pygame version supporting pygame.transform (pygame-1.1+) """
-
-
-def aspect_scale(img, (bx, by)):
-    """ Scales 'img' to fit into box bx/by.
-     This method will retain the original image's aspect ratio """
-    ix,iy = img.get_size()
-    if ix > iy:
-        # fit to width
-        scale_factor = bx/float(ix)
-        sy = scale_factor * iy
-        if sy > by:
-            scale_factor = by/float(iy)
-            sx = scale_factor * ix
-            sy = by
-        else:
-            sx = bx
-    else:
-        # fit to height
-        scale_factor = by/float(iy)
-        sx = scale_factor * ix
-        if sx > bx:
-            scale_factor = bx/float(ix)
-            sx = bx
-            sy = scale_factor * iy
-        else:
-            sy = by
-
-    return pygame.transform.scale(img, (int(sx), int(sy)))
+def aspect_scale(image, (bx, by)):
+    scale_rect = pygame.Rect(0, 0, bx, by)
+    image_rect = image.get_rect()
+    return pygame.transform.scale(image, image_rect.fit(scale_rect).size)
 
 
 def CreateVectorFromCoordinates((x1, y1), (x2, y2)):
@@ -65,3 +22,29 @@ def rot_center(image, angle):
     rot_rect.center = rot_image.get_rect().center
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
+
+
+def reverse_clamp(smaller_rect, larger_rect):
+    if not larger_rect.contains(smaller_rect):
+        new_rect = larger_rect.copy()
+        if smaller_rect.left <= larger_rect.left:
+            new_rect.left = smaller_rect.left
+        elif smaller_rect.right >= larger_rect.right:
+            new_rect.right = smaller_rect.right
+        if smaller_rect.top <= larger_rect.top:
+            new_rect.top = smaller_rect.top
+        elif smaller_rect.bottom >= larger_rect.bottom:
+            new_rect.bottom = smaller_rect.bottom
+        return new_rect
+
+
+def reverse_clamp_ip(smaller_rect, larger_rect):
+    if not larger_rect.contains(smaller_rect):
+        if smaller_rect.left <= larger_rect.left:
+            larger_rect.left = smaller_rect.left
+        elif smaller_rect.right >= larger_rect.right:
+            larger_rect.right = smaller_rect.right
+        if smaller_rect.top <= larger_rect.top:
+            larger_rect.top = smaller_rect.top
+        elif smaller_rect.bottom >= larger_rect.bottom:
+            larger_rect.bottom = smaller_rect.bottom
